a7e6475e10363bb1bccb38f7223aceac6c470b60
Nuno da Costa <nmcosta89@gmail.com>
2020-09-10 10:59:47 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 15:50:25 2020
+
+@author: nmc_costa
+"""
+
+import os
+from setuptools import find_packages, setup
+
+setup(
+    name='violent_action',
+    version='0.5dev',
+    long_description=open('README.md').read(),
+    packages=find_packages()
+)
