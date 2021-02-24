48693eb6c5e38ee83d3028532d76e79389b6e932
Nuno da Costa <nmcosta89@gmail.com>
2021-02-24 12:15:22 +0000

    annotate update, fairmot update

---
+++ fairmot/ENSAIOS/inference_demo/inference_demo_linux.bat
@@ -0,0 +1,9 @@
+"/home/nmc_costa/miniconda3/envs/violent_action/bin/python" "/mnt/c/Users/nmc_costa/google_drive/projects/bosch_P19/research/python_ws/violent_action/fairmot/FairMOT/src/demo.py" \
+mot \
+--load_model="/mnt/c/Users/nmc_costa/google_drive/projects/bosch_P19/research/python_ws/violent_action/fairmot/FairMOT/models/fairmot_dla34.pth" \
+--conf_thres="0.4" \
+--input-video="/mnt/d/external_datasets/SAMPLES/MOLA_VIDEOS_1/VID_20200903_112030.mp4" \
+--output-root="/mnt/d/external_datasets/INFERENCES/fairmot/demo_samples/detect/VID_20200903_112030"
+
+
+
