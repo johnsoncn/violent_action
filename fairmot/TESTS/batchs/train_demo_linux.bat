76cc28f391face730db24d3222e9c0ae1f9729a7
Nuno da Costa <nmcosta89@gmail.com>
2021-03-10 08:26:02 +0000

    folder update 3

---
+++ fairmot/TESTS/batchs/train_demo_linux.bat
@@ -0,0 +1,18 @@
+"/home/nmc_costa/miniconda3/envs/violent_action/bin/python" "/mnt/c/Users/nmc_costa/google_drive/projects/bosch_P19/research/python_ws/violent_action/fairmot/FairMOT/src/train.py" \
+mot \
+--dataset="TAO_TEST" \
+--exp_id="TAO_TEST_1" \
+--load_model="" \
+--gpus="0" \
+--batch_size="16" \
+--num_epochs="60" \
+--num_workers="24" \
+--lr_step="50" \
+--trainval \
+--val_intervals="10" \
+--data_dir="/mnt/d/external_datasets/FAIRMOTESTS/fairmotformat/TAO_TEST" \
+--data_cfg="/mnt/d/external_datasets/FAIRMOTESTS/fairmotformat/TAO_TEST/cfg.json"
+
+
+
+
