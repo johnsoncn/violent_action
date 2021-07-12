734c8f925f7f4c60a9ac220d508b5dd5300b5e09
Nuno da Costa <nmcosta89@gmail.com>
2021-07-12 17:19:56 +0100

    json2mmaction2 update

--- AR/image_based/MMACTION2/json2mmaction2/json2mmaction2.py
+++ AR/image_based/MMACTION2/json2mmaction2/json2mmaction2.py
@@ -1,5 +1,5 @@
 import json
-from shutil import copyfile
+from shutil import copy_imagesfile
 import os
 import argparse
 
@@ -20,11 +20,11 @@ def make_folders(path='../out/'):
 
 
 # Convert motionLab JSON file into  labels --------------------------------
-def convert_mola_json(datasets_root_dir=None, json_dir='../mola/annotations/', outdir='out/', copy_images=True, only_labels=False, img_number=None, track_id=None):
+def convert_mola_json(datasets_root_dir=None, json_dir='../mola/annotations/', outdir='out/', copy_images=True, copy_videos=False, only_labels=False, img_number=None, track_id=None):
     '''
-    #WARNING copy_images="1" #1=copy images from original datasets, easiest way to create  dataset
-    # "0"= don't copy images use the ones from original #WARNING labels need to be organized
-    » copy labels to the same root dir as "images" in original dataset and also have the same path as the images
+    #WARNING copy_images="1" #1=copy_images images from original datasets, easiest way to create  dataset
+    # "0"= don't copy_images images use the ones from original #WARNING labels need to be organized
+    » copy_images labels to the same root dir as "images" in original dataset and also have the same path as the images
 
     # Define label paths as a function of image paths
     sa, sb = os.sep + 'images' + os.sep, os.sep + 'labels' + os.sep  # /images/, /labels/ substrings
@@ -37,25 +37,35 @@ def convert_mola_json(datasets_root_dir=None, json_dir='../mola/annotations/', o
 
     # Import json
     for json_file in sorted(jsons):
-        labeldir_path = 'labels_with_ids/%s/' % Path(json_file).stem  # folder name (train, val, test) remove other info
-        imgdir_path = 'images/%s/' % Path(json_file).stem  # folder name (train, val, test) remove other info
-        dir_label = outdir + labeldir_path
-        os.mkdir(dir_label)
+        # make dirs
+        videodir_path = 'videos_%s/' % Path(json_file).stem  # folder name (train, val, test) remove other info
+        imgdir_path = 'rawframes_%s/' % Path(json_file).stem  # folder name (train, val, test) remove other info
+        dir_video = outdir + videodir_path
+        os.mkdir(dir_video)
         dir_img = outdir + imgdir_path
         os.mkdir(dir_img)
+
+        #json file
         with open(json_file) as f:
             data = json.load(f)
 
-        # image array
-        img_a = []
-        save_img_a= []
-        # label dict
-        lb_d = {}
+        # image lists
+        img_l = []
+        save_img_l= []
+        imglist = []
+        
+        # video lists
+        video_l = []
+        videolist = []
+
         # image counter
         img_counter = 0
 
         # Create image dict {id: image}
         images = {'%g' % x['id']: x for x in data['images']}
+
+        # Create video dict {id: video}
+        videos = {'%g' % x['id']: x for x in data['videos']}
 
         # WRITE
         method="for"
@@ -66,18 +76,20 @@ def convert_mola_json(datasets_root_dir=None, json_dir='../mola/annotations/', o
         tid=0
         if method=="for":
             for x in tqdm(data['annotations'], desc='Annotations %s' % json_file):
-                try:
-                    if x['iscrowd']: continue
-                except:
-                    print('missing "iscrowd" key')
+                image_id='%g' % x['image_id']
+                img = images[image_id]
+                h, w, imgf = img['height'], img['width'], img['file_name']
+                _, img_ext = os.path.splitext(imgf)
+                img_fn = Path(imgf).stem
+                img_new_fn = image_id #original filename - if the images are not copied mantain original path
 
-                img = images['%g' % x['image_id']]
-                h, w, f = img['height'], img['width'], img['file_name']
-                _, img_ext = os.path.splitext(f)
-                img_fn = Path(f).stem
-                img_new_fn = img_fn #original filename - if the images are not copied mantain original path
-                if copy_images: img_new_fn = '%g' % (x['image_id'])  # WARNING - it needs to be the an id
-                if only_labels: img_new_fn = '%g' % (x['image_id'])  # WARNING - it needs to be the an id
+
+                video_id = '%g' % img["video_id"]
+                video = videos[video_id]
+                videof= video["name"]
+                video_fn = Path(videof).stem
+                video_new_fn = video_id
+
 
                 # The Labelbox bounding box format is [top left x, top left y, width, height] | [x,y,w,h]
                 box = np.array(x['bbox'], dtype=np.float64)
@@ -86,74 +98,62 @@ def convert_mola_json(datasets_root_dir=None, json_dir='../mola/annotations/', o
                 box[[1, 3]] /= h  # normalize y & h
 
                 if (box[2] > 0.) and (box[3] > 0.):  # if w > 0 and h > 0
-                    # write images - 1st because if copy fails the
-                    if not str(x['image_id']) in img_a: # don't repeat write images #if not (any([True for im in img_a if im.find(str(x['image_id'])) > -1])):
-                        img_a.append(str(x['image_id']))  # all images
-                        # copy imgs
+                    # write images - 1st because if copy_images fails the
+                    if not image_id in img_l: # don't repeat write images #if not (any([True for im in img_l if im.find(str(x['image_id'])) > -1])):
+                        img_l.append(image_id)  # all images
+                        # copy_images
                         if copy_images or only_labels:
-                            src = os.path.join(datasets_root_dir, f)
+                            src = os.path.join(datasets_root_dir, imgf)
                             dst = os.path.join(dir_img, img_new_fn + img_ext)
                             try:
-                                if copy_images: copyfile(src, dst)  # missing files
+                                if copy_images: copy_imagesfile(src, dst)  # missing files
                                 if only_labels and not os.path.exists(src): raise
+                                save_img_l.append(image_id)  # only images that are saved
                             except:
-                                img_a[-1] = img_a[-1] + '_MISSING'
-                                print("\n>> missing : {}".format(Path(os.path.join(datasets_root_dir, f))))
-                                print(img_a[-1])
-                                continue #if image missing from dataset when copy dont write nothing more
-                        # save img path
-                        with open(outdir + Path(json_file).stem + '.txt', 'a+') as file:
-                            img_new_path = '%s\n' % Path(os.path.join(datasets_root_dir, f))  # original datasets path
-                            if copy_images or only_labels:  #FairMOT.src.lib.cfg -jsons
-                                img_new_path = '%s\n' % Path(imgdir_path + img_new_fn + img_ext)  # local path
-                                # img_new_path = '%s\n' % Path(outdir + imgdir_path + img_new_fn + img_ext)  # global path
-                            file.write(img_new_path)
+                                img_l[-1] = img_l[-1] + '_MISSING'
+                                print("\n>> missing : {}".format(Path(os.path.join(datasets_root_dir, imgf))))
+                                print(img_l[-1])
+                                continue #if image missing from dataset when copy_images dont write nothing more
+                        # rawframe annotation file list: json to txt [ frame_directory total_frames label  ]
+                        imgline = "{} {} {}".format(category + '/' + video_id + '/' + img_new_fn, total_frames, label)
+                        imglist.append(imgline)
                         img_counter += 1
-                        lb_d[str(x['image_id'])]=[]
-                        save_img_a.append(str(x['image_id']))  # only images that are saved
-                        print(str(x['image_id']))
-                    # write labels
-                    if  (str(x['image_id']) in save_img_a) and (not str(box) in lb_d[str(x['image_id'])]):  # don't write same labels (even if annotations are duplicated)
-                        lb_d[str(x['image_id'])].append(str(box))
-                        if copy_images or only_labels:
-                            with open(dir_label + img_new_fn + '.txt', 'a') as file:
-                                if track_id: file.write('{:d} {:d} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(x['category_id'] - 1, x[track_id], *box)) #category_id starts with 1, track_id starts in 0
-                                else: file.write('{:d} {:d} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(x['category_id'] - 1, tid, *box))
-                                # label_str = '{:d} {:d} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(class_id, tid_curr, x / img_width, y / img_height, w / img_width, h / img_height)
-                        else:
-                            with open(dir_label + os.path.splitext(f)[0]+ '.txt', 'a+') as file:
-                                if track_id:
-                                    file.write('{:d} {:d} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(x['category_id'] - 1,
-                                                                                                x[track_id],
-                                                                                                *box))  # category_id starts with 1, track_id starts in 0
-                                else:
-                                    file.write(
-                                        '{:d} {:d} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(x['category_id'] - 1, tid,
-                                                                                         *box))
-                        tid+=1
+                        print(image_id)
+                    # write videos
+                    if not video_id in video_l:  # don't repeat write images #if not (any([True for im in img_l if im.find(str(x['image_id'])) > -1])):
+                        video_l.append(video_id)  # all images
+                        # copy_videos
+                        if copy_videos or only_labels:
+                            src = os.path.join(datasets_root_dir, imgf)
+                            dst = os.path.join(dir_img, img_new_fn + img_ext)
+                            try:
+                                if copy_images: copy_imagesfile(src, dst)  # missing files
+                                if only_labels and not os.path.exists(src): raise
+                                save_img_l.append(image_id)  # only images that are saved
+                            except:
+                                img_l[-1] = img_l[-1] + '_MISSING'
+                                print("\n>> missing : {}".format(Path(os.path.join(datasets_root_dir, videof))))
+                                print(img_l[-1])
+                                continue  # if image missing from dataset when copy_images dont write nothing more
+                        #video annotation file list: json to txt [fielpath label]
+                        videoline = "{} {} {}".format(category + '/' + video_id + '/' + img_new_fn, total_frames, label)
+                        videolist.append(videoline)
+                        print(video_id)
 
                 # STOP
                 if img_number and img_counter >= img_number: break
+            #save imglist : mola_{train,val}_rawframes.txt
+            with open(outdir + Path(json_file).stem + '.txt', 'a+') as file:
+                for i in imglist:
+                    file.write(i)
+            #save videolist : mola_{train,val}_rawframes.txt
+            with open(outdir + Path(json_file).stem + '.txt', 'a+') as file:
+                for v in videolist:
+                    file.write(v)
 
-        #Config file: root of export dataset; train paths to use
-        cfg={
-            "root": os.path.dirname(outdir),
-            "train":
-                {
-                    "train": outdir+"train.txt",
-                    "val": outdir+ "val.txt",
-                },
-            "test_emb":
-                {
-                    "test": outdir+"test.txt"
-                },
-            "test":
-                {
-                    "test": outdir+"test.txt"
-                }
-        }
-        with open(outdir + 'cfg.json', 'w') as f:
-            json.dump(cfg, f)
+
+
+
 
         stop = time.time()
         elapsed=stop-start
@@ -172,9 +172,9 @@ if __name__ == '__main__':
                         default="D:/external_datasets/MOLA/fairmotformat/mola_train/",
                         help='fairmotformat dataset output path')
     parser.add_argument('--img_number', type=int, default=None, help='number of images to convert. None=convert all')
-    parser.add_argument('--copy_images', type=int, default=0, help='copy images to folder /images and add new path to .txt . If 0 no image is copied and .txt has the original paths')
+    parser.add_argument('--copy_images', type=int, default=0, help='copy_images images to folder /images and add new path to .txt . If 0 no image is copied and .txt has the original paths')
     parser.add_argument('--only_labels', type=int, default=0,
-                        help='write labels only as if you were copying images to folder /images and add new path to .txt .')
+                        help='write labels only as if you were copy_imagesing images to folder /images and add new path to .txt .')
     parser.add_argument('--track_id', type=str,
                         default=None,
                         help='track id json key in annotations')
